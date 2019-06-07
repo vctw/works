93a456c5ac9f8fac05fb23f0c559a89327eeac02
vt <vt@vct.ink>
2019-06-08 00:30:37 +0800

    reader init

---
+++ reader/src/main/java/pers/vct/reader/ReaderApplication.java
@@ -0,0 +1,13 @@
+package pers.vct.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderApplication.class, args);
+	}
+
+}
